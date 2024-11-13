c03f3f329b5c4500b882d76ab898cb3036cea7f0
Christopher Plonski <christopherplonski@gmail.com>
2024-11-13 14:55:39 -0600

    Implemented Add-ons #2 + #3

--- CSV_Calculator.cpp
+++ CSV_Calculator.cpp
@@ -28,6 +28,34 @@ optional<int> try_get_int_from_string(const string stringToConvert)
     return nullopt;
 }
 
+string vector_data_as_string(vector<int> vector) {
+    string stringVectorData = "";
+
+    stringVectorData += "{";
+    const int VECTOR_SIZE = vector.size();
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        stringVectorData += to_string(vector.at(i));
+
+        if (i < VECTOR_SIZE - 1) {
+            stringVectorData += ", ";
+        }
+    }
+
+    stringVectorData += "}";
+
+    return stringVectorData;
+}
+
+bool is_value_in_vector(vector<int> vector, int valueToFind) {
+    for (int i = 0; i < vector.size(); i++) {
+        if (vector.at(i) == valueToFind) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 void add_string_line_int_values_to_vector(string line, vector<int> &vectorToOutputTo) {
     bool keepAddingValues = true;
     
@@ -96,18 +124,81 @@ double get_vector_median(const vector<int> dataVector) {
     }
 }
 
+vector<int> get_vector_mode(const vector<int> dataVector) {
+    vector<int> modeVector = vector<int>();
+    int numberOfTimeModesAppear = 0;
+
+    int currentAnalyzingIntValue = 0;
+    int numberOfTimesCurrentAnalyzingValueAppears = 0;
+
+    for (int i = 0; i < dataVector.size(); i++) {
+        int valueAtIndex = dataVector.at(i);
+
+        if (currentAnalyzingIntValue != valueAtIndex) {
+            currentAnalyzingIntValue = valueAtIndex;
+            numberOfTimesCurrentAnalyzingValueAppears = 0;
+        }
+
+        numberOfTimesCurrentAnalyzingValueAppears++;
+
+        if (is_value_in_vector(modeVector, valueAtIndex)) {
+            // This value is now in the data set more than all the other modes, so clear it and have this as the only mode
+            modeVector.clear();
+            modeVector.push_back(valueAtIndex);
+            numberOfTimeModesAppear++;
+        }
+        else if (numberOfTimesCurrentAnalyzingValueAppears == numberOfTimeModesAppear) {
+            // This value is now at the same level as all the other modes, so add it to the list.
+            modeVector.push_back(valueAtIndex);
+        }
+        else if (numberOfTimeModesAppear == 0) {
+            // This is the beginning of the mode analyzing, which means this value has to be the current mode no matter what.
+            modeVector.push_back(valueAtIndex);
+            numberOfTimeModesAppear++;
+        }
+    }
+
+    return modeVector;
+}
+
+void sort_csv_file_values(vector<int> &csvFileDataVector) {
+    cout << "Data before sort: " << vector_data_as_string(csvFileDataVector) << endl;
+
+    // Ex: {10, 5, 3, 7, 1, 12} - Size = 6
+    const int DATA_SIZE = csvFileDataVector.size();
+
+    for (int i = 0; i < DATA_SIZE - 1; i++) {
+        bool isDataPerfectlySorted = true;
+
+        for (int j = 0; j < DATA_SIZE - 1; j++) {
+            int value1 = csvFileDataVector.at(j);
+            int value2 = csvFileDataVector.at(j + 1);
+
+            if (value2 < value1) {
+                isDataPerfectlySorted = false;
+                csvFileDataVector[j] = value2;
+                csvFileDataVector[j + 1] = value1;
+                //csvFileDataVector.assign(j, value2);
+                //csvFileDataVector.assign(j + 1, value1);
+            }
+        }
+
+        if (isDataPerfectlySorted) {
+            break;
+        }
+    }
+
+    cout << "Data after sort: " << vector_data_as_string(csvFileDataVector) << endl;
+}
+
 int main() {
 	cout << "Enter the .csv filePath: ";
 	string userFilePathInput;
 	cin >> userFilePathInput;
 
     vector<int> csvFileDataVector = get_csv_file_values(userFilePathInput);
+    sort_csv_file_values(csvFileDataVector);
 
-    //cout << "Data: {";
-    //for (int integer : csvFileData) {
-    //    cout << integer << ", ";
-    //}
-    //cout << "}" << endl;
-
-    cout << "Data median: " << get_vector_median(csvFileDataVector);
+    cout << "Data median: " << get_vector_median(csvFileDataVector) << endl;
+    cout << "Data mode: " << vector_data_as_string(get_vector_mode(csvFileDataVector)) << endl;
 }
